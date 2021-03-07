ea04384d3c5716a86dede451be6a6753bf3db29d
reemaot <75852614+reemaot@users.noreply.github.com>
2021-03-07 22:46:58 +0300

    Initial commit

---
+++ app/src/main/java/com/example/lab5ex1/sushi1.java
@@ -0,0 +1,14 @@
+package com.example.lab5ex1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sushi1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sushi1);
+    }
+}
